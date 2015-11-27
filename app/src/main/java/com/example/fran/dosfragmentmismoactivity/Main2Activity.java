19c9bfa7d110e2d0ca15903fe1f51fd3feba0bbd
Fran <fabrilalvarez@danielcastelao.org>
2015-11-27 12:32:03 +0100

    Añadido boton que interactua con activities

--- app/src/main/java/com/example/fran/dosfragmentmismoactivity/Main2Activity.java
+++ app/src/main/java/com/example/fran/dosfragmentmismoactivity/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.fran.dosfragmentmismoactivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        getIntent();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                String mensaje = data.getStringExtra(MainActivity.MENSAJE);
+                TextView edtmensaje = (TextView) findViewById(R.id.txtv2_fragment_b_land);
+                edtmensaje.setText(mensaje);
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/fran/dosfragmentmismoactivity/MainActivity.java
+++ app/src/main/java/com/example/fran/dosfragmentmismoactivity/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.fran.dosfragmentmismoactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static String MENSAJE = "ENVIAR MENSAJE BOTON PICADO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
             TextView txtv1 = (TextView) findViewById(R.id.txtv_fragment_a_port);
             txtv1.setText("Land");
         }
+    }
+
+    public void irActivity2(View v) {
+        FragmentB fragment = (FragmentB) getFragmentManager().findFragmentById(R.id.id_fragment_b);
+        if (fragment == null || !fragment.isInLayout()) {
+            // start new Activity
+            Intent irActivity2 = new Intent(this, Main2Activity.class);
+            startActivityForResult(irActivity2, 1);
+        } else {
+            //fragment.update(...);
+            TextView txtv2 = (TextView) findViewById(R.id.txtv2_fragment_b_land);
+            txtv2.setText("HAS PICADO EL BOTON!");
+            Intent enActivity2 = new Intent(this, Main2Activity.class);
+            enActivity2.putExtra(MENSAJE, txtv2.getText().toString());
+
+        }
+
     }
 }
